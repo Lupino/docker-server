3a8efa6579e83b60a33f2aa92525bb93207517a5
Ubasic <lmjubuntu@gmail.com>
2013-09-17 23:00:39 +0800

    add Container, User, UserContainer

---
+++ docker/drivers/store.py
@@ -0,0 +1,66 @@
+from crawl.drivers.store import *
+
+class Container(Model):
+    table_name = '{}container'.format(prefix)
+
+    create_table_oursql_sql = '''
+        CREATE TABLE IF NOT EXISTS `{}container` (
+          `container_id` varchar(32) NOT NULL,
+          `ssh_port` int(5) unsigned NOT NULL DEFAULT '0',
+          `www_port` int(5) unsigned NOT NULL DEFAULT '0',
+          `created_at` int(10) unsigned NOT NULL DEFAULT '0',
+          `stop_at` int(10) unsigned NOT NULL DEFAULT '0',
+          `last_startup` int(10) unsigned NOT NULL DEFAULT '0',
+          PRIMARY KEY (`container_id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;'''.format(prefix)
+
+    columns = [
+        {'name': 'container_id', 'type': 'str', 'primary': True},
+        {'name': 'ssh_port',     'type': 'int'},
+        {'name': 'www_port',     'type': 'int'},
+        {'name': 'created_at',   'type': 'int'},
+        {'name': 'stop_at',      'type': 'int'},
+        {'name': 'last_startup', 'type': 'int'}
+    ]
+
+container = Container()
+
+class UserContainer(Model):
+    table_name = '{}user_container'.format(prefix)
+
+    create_table_oursql_sql = '''
+        CREATE TABLE IF NOT EXISTS `{}user_container` (
+          `user_id` int(10) unsigned NOT NULL,
+          `container_id` varchar(32) NOT NULL,
+          PRIMARY KEY (`user_id`,`container_id`),
+          KEY `container_id` (`container_id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;'''.format(prefix)
+
+    columns = [
+        {'name': 'user_id',      'type': 'int', 'primary': True},
+        {'name': 'container_id', 'type': 'str', 'primary': True}
+    ]
+
+user_container = UserContainer()
+
+class User(Model):
+    table_name = '{}user'.format(prefix)
+
+    create_table_oursql_sql = '''
+        CREATE TABLE IF NOT EXISTS `{}user` (
+          `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+          `username` varchar(50) NOT NULL,
+          `passwd` varchar(32) NOT NULL,
+          `email` varchar(100) NOT NULL,
+          PRIMARY KEY (`user_id`),
+          UNIQUE KEY `username` (`username`,`email`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;'''.format(prefix)
+
+    columns = [
+        {'name': 'user_id',  'type': 'int', 'primary': True},
+        {'name': 'username', 'type': 'str', 'unique': True},
+        {'name': 'passwd',   'type': 'str'},
+        {'name': 'email',    'type': 'str', 'unique': True}
+    ]
+
+user = User()
