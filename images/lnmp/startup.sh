9c56f0af0ab5b8903ae59ab4f060abf13cdf3ad0
Ubasic <lmjubuntu@gmail.com>
2013-10-15 07:45:01 +0800

    Add lnmp image

---
+++ images/lnmp/startup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Add docker user and generate a random password with 12 characters that includes at least one capital letter and number.
+DOCKER_PASSWORD=`pwgen -c -n -1 12`
+echo User: docker Password: $DOCKER_PASSWORD
+DOCKER_ENCRYPYTED_PASSWORD=`perl -e 'print crypt('"$DOCKER_PASSWORD"', "aa"),"\n"'`
+useradd -m -d /home/docker -p $DOCKER_ENCRYPYTED_PASSWORD docker
+sed -Ei 's/adm:x:4:/docker:x:4:docker/' /etc/group
+adduser docker sudo
+
+# Set the default shell as bash for docker user.
+chsh -s /bin/bash docker
+
+ln -s /proc/mounts /etc/mtab
+
+cat << EOF > /etc/hosts
+127.0.0.1   localhost $HOSTNAME
+
+::1 ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts
+EOF
+
+ADMIN_PASS=${ADMIN_PASS:-$DOCKER_PASSWORD}
+MYSQL_CFG_URL=${MYSQL_CFG_URL:-}
+DATA_DIR=${DATA_DIR:-/var/lib/mysql}
+MYSQL_OPTS=${MYSQL_OPTS:-}
+
+if [ ! -z "$MYSQL_CFG_URL" ] ; then
+    wget $MYSQL_CFG_URL -O /etc/mysql/my.cnf
+fi
+
+sed -i 's/^bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/my.cnf
+
+if [ ! -e "$DATA_DIR/mysql" ] ; then
+    mysql_install_db --datadir=$DATA_DIR
+fi
+
+# create user
+mysqld --datadir=$DATA_DIR --verbose &
+sleep 3
+echo "* Setting privileges..."
+echo "grant all privileges on *.* to root@'%' identified by \"$ADMIN_PASS\" with grant option;" | mysql -u root
+echo "* Setting password..."
+mysqladmin -u root password "$ADMIN_PASS"
+
+killall mysqld
+sleep 2
+
+mysqld --datadir=$DATA_DIR --verbose $OPTS &
+
+/etc/init.d/php5-fpm start
+/etc/init.d/nginx start
+# Start the ssh service
+/usr/sbin/sshd -D
